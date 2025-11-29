829ec12ebf1f38975c5a6523a5b765c4e2d68fa9
Joshua Anderson <andersmmg@gmail.com>
2025-11-28 17:02:50 -0700

    basic nav sidebar

--- src/lib/components/app-sidebar.svelte
+++ src/lib/components/app-sidebar.svelte
@@ -1,5 +1,149 @@
 <script lang="ts">
- import * as Sidebar from "$lib/components/ui/sidebar";
+  import * as Sidebar from "$lib/components/ui/sidebar/index.js";
+  import {
+    Book,
+    FileText,
+    Users,
+    Settings,
+    PenTool,
+    Bookmark,
+    Search,
+    Home,
+  } from "@lucide/svelte";
 </script>
 
-hello
+<Sidebar.Root>
+  <Sidebar.Content>
+    <Sidebar.Header>
+      <Sidebar.MenuButton>
+        <Book class="size-4" />
+        <span>Novel Writer</span>
+      </Sidebar.MenuButton>
+    </Sidebar.Header>
+
+    <Sidebar.Group>
+      <Sidebar.GroupLabel>Navigation</Sidebar.GroupLabel>
+      <Sidebar.GroupContent>
+        <Sidebar.Menu>
+          <Sidebar.MenuItem>
+            <Sidebar.MenuButton>
+              {#snippet child({ props })}
+                <a href="/" {...props}>
+                  <Home class="size-4" />
+                  <span>Dashboard</span>
+                </a>
+              {/snippet}
+            </Sidebar.MenuButton>
+          </Sidebar.MenuItem>
+
+          <Sidebar.MenuItem>
+            <Sidebar.MenuButton>
+              {#snippet child({ props })}
+                <a href="/novels" {...props}>
+                  <Book class="size-4" />
+                  <span>My Novels</span>
+                </a>
+              {/snippet}
+            </Sidebar.MenuButton>
+          </Sidebar.MenuItem>
+
+          <Sidebar.MenuItem>
+            <Sidebar.MenuButton>
+              {#snippet child({ props })}
+                <a href="/chapters" {...props}>
+                  <FileText class="size-4" />
+                  <span>Chapters</span>
+                </a>
+              {/snippet}
+            </Sidebar.MenuButton>
+          </Sidebar.MenuItem>
+
+          <Sidebar.MenuItem>
+            <Sidebar.MenuButton>
+              {#snippet child({ props })}
+                <a href="/characters" {...props}>
+                  <Users class="size-4" />
+                  <span>Characters</span>
+                </a>
+              {/snippet}
+            </Sidebar.MenuButton>
+          </Sidebar.MenuItem>
+
+          <Sidebar.MenuItem>
+            <Sidebar.MenuButton>
+              {#snippet child({ props })}
+                <a href="/bookmarks" {...props}>
+                  <Bookmark class="size-4" />
+                  <span>Bookmarks</span>
+                </a>
+              {/snippet}
+            </Sidebar.MenuButton>
+          </Sidebar.MenuItem>
+        </Sidebar.Menu>
+      </Sidebar.GroupContent>
+    </Sidebar.Group>
+
+    <Sidebar.Separator />
+
+    <Sidebar.Group>
+      <Sidebar.GroupLabel>Writing Tools</Sidebar.GroupLabel>
+      <Sidebar.GroupContent>
+        <Sidebar.Menu>
+          <Sidebar.MenuItem>
+            <Sidebar.MenuButton>
+              {#snippet child({ props })}
+                <a href="/editor" {...props}>
+                  <PenTool class="size-4" />
+                  <span>Editor</span>
+                </a>
+              {/snippet}
+            </Sidebar.MenuButton>
+          </Sidebar.MenuItem>
+
+          <Sidebar.MenuItem>
+            <Sidebar.MenuButton>
+              {#snippet child({ props })}
+                <a href="/search" {...props}>
+                  <Search class="size-4" />
+                  <span>Search</span>
+                </a>
+              {/snippet}
+            </Sidebar.MenuButton>
+          </Sidebar.MenuItem>
+        </Sidebar.Menu>
+      </Sidebar.GroupContent>
+    </Sidebar.Group>
+
+    <Sidebar.Separator />
+
+    <Sidebar.Group>
+      <Sidebar.GroupLabel>System</Sidebar.GroupLabel>
+      <Sidebar.GroupContent>
+        <Sidebar.Menu>
+          <Sidebar.MenuItem>
+            <Sidebar.MenuButton>
+              {#snippet child({ props })}
+                <a href="/settings" {...props}>
+                  <Settings class="size-4" />
+                  <span>Settings</span>
+                </a>
+              {/snippet}
+            </Sidebar.MenuButton>
+          </Sidebar.MenuItem>
+        </Sidebar.Menu>
+      </Sidebar.GroupContent>
+    </Sidebar.Group>
+
+    <Sidebar.Footer>
+      <Sidebar.Menu>
+        <Sidebar.MenuItem>
+          <Sidebar.MenuButton>
+            <span class="text-xs text-muted-foreground">v1.0.0</span>
+          </Sidebar.MenuButton>
+        </Sidebar.MenuItem>
+      </Sidebar.Menu>
+    </Sidebar.Footer>
+  </Sidebar.Content>
+
+  <Sidebar.Rail />
+</Sidebar.Root>
--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -6,37 +6,16 @@
   import "../app.css";
   import AppSidebar from "$lib/components/app-sidebar.svelte";
   import AppMenubar from "$lib/components/app-menubar.svelte";
-  import * as Resizable from "$lib/components/ui/resizable/index.js";
 
   let sidebarOpen = $state(true);
-  let sidebarPane: ReturnType<typeof Resizable.Pane>;
-
-  $effect(() => {
-    if (sidebarOpen) {
-      sidebarPane?.expand();
-    } else {
-      sidebarPane?.collapse();
-    }
-  });
 </script>
 
 <ModeWatcher />
-  <AppMenubar bind:sidebarOpen />
+<AppMenubar bind:sidebarOpen />
 
-  <main class="w-full h-[calc(100svh-2.5rem)]">
-    <Resizable.PaneGroup direction="horizontal">
-      <Resizable.Pane
-        defaultSize={25}
-        collapsible={true}
-        bind:this={sidebarPane}
-        onCollapse={() => (sidebarOpen = false)}
-        onExpand={() => (sidebarOpen = true)}
-      >
-        <AppSidebar />
-      </Resizable.Pane>
-      <Resizable.Handle />
-      <Resizable.Pane defaultSize={75} class="overflow-y-auto">
-        {@render children?.()}
-      </Resizable.Pane>
-    </Resizable.PaneGroup>
+<Sidebar.Provider open={sidebarOpen}  class="min-h-[calc(100svh-2.25rem)]">
+  <AppSidebar />
+  <main>
+    {@render children?.()}
   </main>
+</Sidebar.Provider>
