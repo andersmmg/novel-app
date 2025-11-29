0ef5126411cbb9e2ef0aebdef9fc798f2c334083
Joshua Anderson <andersmmg@gmail.com>
2025-11-29 14:14:18 -0700

    sidebar ui update

--- src/lib/components/app-sidebar.svelte
+++ src/lib/components/app-sidebar.svelte
@@ -9,13 +9,37 @@
 		Bookmark,
 		Search,
 		House,
+		Folder,
+		FolderOpen,
+		File,
 	} from "@lucide/svelte";
+
+	let notesOpen = false;
+	let subFolder1Open = false;
+	let subFolder2Open = false;
+	let subFolder3Open = false;
+
+	function toggleNotes() {
+		notesOpen = !notesOpen;
+	}
+
+	function toggleSubFolder1() {
+		subFolder1Open = !subFolder1Open;
+	}
+
+	function toggleSubFolder2() {
+		subFolder2Open = !subFolder2Open;
+	}
+
+	function toggleSubFolder3() {
+		subFolder3Open = !subFolder3Open;
+	}
 </script>
 
-<Sidebar.Root class="mt-9">
-	<Sidebar.Content class="border-l">
+<Sidebar.Root class="mt-9 max-h-[calc(100svh-2.25rem)]">
+	<Sidebar.Content class="border-l border-b  overflow-x-hidden">
+		<!-- My Novels -->
 		<Sidebar.Group>
-			<Sidebar.GroupLabel>Navigation</Sidebar.GroupLabel>
 			<Sidebar.GroupContent>
 				<Sidebar.Menu>
 					<Sidebar.MenuItem>
@@ -28,7 +52,17 @@
 							{/snippet}
 						</Sidebar.MenuButton>
 					</Sidebar.MenuItem>
+				</Sidebar.Menu>
+			</Sidebar.GroupContent>
+		</Sidebar.Group>
 
+		<Sidebar.Separator />
+
+		<!-- Story Elements Group -->
+		<Sidebar.Group>
+			<Sidebar.GroupLabel>Story Elements</Sidebar.GroupLabel>
+			<Sidebar.GroupContent>
+				<Sidebar.Menu>
 					<Sidebar.MenuItem>
 						<Sidebar.MenuButton>
 							{#snippet child({ props })}
@@ -52,21 +86,129 @@
 					</Sidebar.MenuItem>
 
 					<Sidebar.MenuItem>
-						<Sidebar.MenuButton>
-							{#snippet child({ props })}
-								<a href="/bookmarks" {...props}>
-									<Bookmark class="size-4" />
-									<span>Bookmarks</span>
-								</a>
-							{/snippet}
-						</Sidebar.MenuButton>
+						<button
+							class="w-full flex items-center gap-2 px-2 py-1 text-sm hover:bg-sidebar-accent rounded-md"
+							onclick={toggleNotes}
+						>
+							{#if notesOpen}
+								<FolderOpen class="size-4" />
+							{:else}
+								<Folder class="size-4" />
+							{/if}
+							<span>Notes</span>
+						</button>
 					</Sidebar.MenuItem>
 				</Sidebar.Menu>
+
+				{#if notesOpen}
+					<Sidebar.Menu class="ml-2">
+						<Sidebar.MenuItem>
+							<Sidebar.MenuButton size="sm">
+								{#snippet child({ props })}
+									<a href="/editor" {...props}>
+										<FileText class="size-4" />
+										<span>Ideas</span>
+									</a>
+								{/snippet}
+							</Sidebar.MenuButton>
+						</Sidebar.MenuItem>
+
+						<Sidebar.MenuItem>
+							<button
+								class="w-full flex items-center gap-2 px-2 py-1 text-xs hover:bg-sidebar-accent rounded-md"
+								onclick={toggleSubFolder1}
+							>
+								{#if subFolder1Open}
+									<FolderOpen class="size-4" />
+								{:else}
+									<Folder class="size-4" />
+								{/if}
+								<span>Magic</span>
+							</button>
+						</Sidebar.MenuItem>
+
+						{#if subFolder1Open}
+							<Sidebar.Menu class="ml-4">
+								<Sidebar.MenuItem>
+									<Sidebar.MenuButton size="sm">
+										{#snippet child({ props })}
+											<a href="#" {...props}>
+												<FileText class="size-4" />
+												<span>Spells</span>
+											</a>
+										{/snippet}
+									</Sidebar.MenuButton>
+								</Sidebar.MenuItem>
+
+								<Sidebar.MenuItem>
+									<button
+										class="w-full flex items-center gap-2 px-2 py-1 text-xs hover:bg-sidebar-accent rounded-md"
+										onclick={toggleSubFolder2}
+									>
+										{#if subFolder2Open}
+											<FolderOpen class="size-4" />
+										{:else}
+											<Folder class="size-4" />
+										{/if}
+										<span>Elements</span>
+									</button>
+								</Sidebar.MenuItem>
+
+								{#if subFolder2Open}
+									<Sidebar.Menu class="ml-4">
+										<Sidebar.MenuItem>
+											<Sidebar.MenuButton size="sm">
+												{#snippet child({ props })}
+													<a href="#" {...props}>
+														<FileText
+															class="size-4"
+														/>
+														<span>Earth</span>
+													</a>
+												{/snippet}
+											</Sidebar.MenuButton>
+										</Sidebar.MenuItem>
+									</Sidebar.Menu>
+								{/if}
+							</Sidebar.Menu>
+						{/if}
+
+						<Sidebar.MenuItem>
+							<button
+								class="w-full flex items-center gap-2 px-2 py-1 text-xs hover:bg-sidebar-accent rounded-md"
+								onclick={toggleSubFolder3}
+							>
+								{#if subFolder3Open}
+									<FolderOpen class="size-4" />
+								{:else}
+									<Folder class="size-4" />
+								{/if}
+								<span>Locations</span>
+							</button>
+						</Sidebar.MenuItem>
+
+						{#if subFolder3Open}
+							<Sidebar.Menu class="ml-4">
+								<Sidebar.MenuItem>
+									<Sidebar.MenuButton size="sm">
+										{#snippet child({ props })}
+											<a href="#" {...props}>
+												<FileText class="size-4" />
+												<span>House</span>
+											</a>
+										{/snippet}
+									</Sidebar.MenuButton>
+								</Sidebar.MenuItem>
+							</Sidebar.Menu>
+						{/if}
+					</Sidebar.Menu>
+				{/if}
 			</Sidebar.GroupContent>
 		</Sidebar.Group>
 
 		<Sidebar.Separator />
 
+		<!-- Writing Tools -->
 		<Sidebar.Group>
 			<Sidebar.GroupLabel>Writing Tools</Sidebar.GroupLabel>
 			<Sidebar.GroupContent>
@@ -92,12 +234,24 @@
 							{/snippet}
 						</Sidebar.MenuButton>
 					</Sidebar.MenuItem>
+
+					<Sidebar.MenuItem>
+						<Sidebar.MenuButton>
+							{#snippet child({ props })}
+								<a href="/bookmarks" {...props}>
+									<Bookmark class="size-4" />
+									<span>Bookmarks</span>
+								</a>
+							{/snippet}
+						</Sidebar.MenuButton>
+					</Sidebar.MenuItem>
 				</Sidebar.Menu>
 			</Sidebar.GroupContent>
 		</Sidebar.Group>
 
 		<Sidebar.Separator />
 
+		<!-- System -->
 		<Sidebar.Group>
 			<Sidebar.GroupLabel>System</Sidebar.GroupLabel>
 			<Sidebar.GroupContent>
